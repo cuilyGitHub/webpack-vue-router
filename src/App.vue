ce542d022e0f80cb4d2f34bb886b1c1c4810df6c
wulibaibao <13366578180@163.com>
2017-08-11 14:03:12 +0800

    更改

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <router-view></router-view>
-    <ul>
-      <li><router-link to="/home">go to home</router-link></li>
-      <li><router-link to="/essential">Essential Links</router-link></li>
-      <li><router-link to="/ecosystem">Ecosystem</router-link></li>
+    <ul class="click-nav">
+      <li ref="clicknav" class="clickNav" v-on:mouseover="overMouse" v-on:mouseout="outMouse"><router-link to="/home">go to home</router-link></li>
+      <li ref="clicknav" class="clickNav" v-on:mouseover="overMouse" v-on:mouseout="outMouse"><router-link to="/essential">Essential Links</router-link></li>
+      <li ref="clicknav" class="clickNav" v-on:mouseover="overMouse" v-on:mouseout="outMouse"><router-link to="/ecosystem">Ecosystem</router-link></li>
     </ul>
   </div>
 </template>
@@ -17,8 +17,17 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
+        clickNav:"clickNav"
     }
-  }
+  },
+    methods:{
+      overMouse(e){
+          e.currentTarget.className='isClickNav';
+      },
+      outMouse(e){
+          e.currentTarget.className='clickNav';
+      }
+    }
 }
 </script>
 
@@ -48,5 +57,12 @@ li {
 
 a {
   color: #42b983;
+  text-decoration: none;
 }
+  .click-nav{
+    color: #333;
+  }
+  .click-nav li a{color: #333;font-family: "Microsoft YaHei";font-weight: 900;}
+  .isClickNav a{color:#fff;background-color: aqua;border: 1px solid #028d4a;}
+  .clickNav a{color:#333;background-color: #fff;}
 </style>
