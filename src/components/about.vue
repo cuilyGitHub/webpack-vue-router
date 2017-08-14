ddf362a06545dfb4d4baa7bead257e2c37902111
wulibaibao <13366578180@163.com>
2017-08-14 13:50:35 +0800

    修改保存

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,14 +1,6 @@
 <template>
     <div class="about-main-wrapper">
-        <div class="about-us-title">
-            <div class="about-us-wrapper">
-                <div class="line-left"></div>
-                <div class="line-center"></div>
-                <div class="line-right"></div>
-                <div class="word">About Us</div>
-            </div>
-            <h2>关于我们</h2>
-        </div>
+        <title-components this-title="关于我们" this-transform="About Us"></title-components>
         <div class="about-content">
             <p>知路是普来科技研发的车联网C2B信息共享平台，产品服务于3000万卡车司机，为卡车司机提供定制化的服务产品，包括但不局限于找油气站、路线规划、路况提醒、油气卡充值、车友互动等汽车后市场服务，为您提供便捷的在途生活。</p>
             <p>人才是普来科技的财富，核心团队均来自于一线互联网公司，在互联网领域有着丰富的研发以及推广、运营经验。员工以90后为主，富有激情、勇于挑战，形成了不断创新的团队精神。怀着对产品的无限热情，我们将不断追求完美！亲爱的知路用户，希望您能从每一个精雕细琢的细节中体会普来团队的用心，体验车主移动互联生活的乐趣。</p>
@@ -16,6 +8,7 @@
     </div>
 </template>
 <script>
+import TitleComponents from '../components/title.vue';
 export default {
     name: 'about',
     data() {
